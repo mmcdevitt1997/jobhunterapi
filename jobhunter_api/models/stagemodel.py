aedd8b751f748a3e6744272b2634e4fcf2b27082
mmcdevitt1997 <30372557+mmcdevitt1997@users.noreply.github.com>
2020-01-19 10:43:45 -0600

    fixed notes for stages

--- jobhunter_api/models/stagemodel.py
+++ jobhunter_api/models/stagemodel.py
@@ -3,6 +3,6 @@ from .jobmodel import JobModel
 
 class StageModel(models.Model):
     name = models.CharField(max_length = 50)
-    note = models.CharField(max_length = 500)
+    note = models.CharField(max_length = 500, blank=True, null=True)
     date = models.DateField()
     job = models.ForeignKey("JobModel", on_delete=models.CASCADE)
